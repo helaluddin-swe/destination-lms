aeb61ca997362c29fb7cde18da4755d4e509cabe
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-01-16 23:03:48 +0600

    s

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {assets} from "../../assets/assets"
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='flex flex-col justify-center items-center '>
+      <h1 className='text-4xl max-w-2xl '>Empower your future with the courses designed to
+      <span className='  text-cyan-500'> fit your choice.</span></h1><img src={assets.sketch} alt="sketch" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, consectetur cupiditate, nam deleniti fugiat ab repellendus, sequi quaerat maiores corporis ducimus? Iste quas odit, aspernatur quae quisquam culpa quia delectus!</p>
     </div>
   )
 }
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,16 +1,22 @@
 
 
+import { useClerk,useUser,UserButton } from '@clerk/clerk-react'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  const {user}=useUser()
+  const {openSignIn}=useClerk()
 
 const isStudentRoute=location.pathname.includes("/course-list")
 
   return (
     <div>
       <div className={`hidden md:flex ${isStudentRoute? "bg-cyan-100/70":"bg-white"} gap-4 items-center justify-between px-10 py-4 `}> <img src={assets.logo} alt="Logo" />
-      <div className='flex justify-around items-center gap-8'> <button className='text-2xl font-bold'> Home</button><button className='bg-blue-500 text-white rounded-full px-7 py-2 hover:bg-blue-400 cursor-pointer'>Be a educator</button></div>
+      <div className='flex justify-around items-center gap-8'> <button className='text-2xl font-bold'> Home</button>
+      {user? <UserButton/>: <button onClick={()=>openSignIn()} className='bg-blue-500 text-white rounded-full px-7 py-2 hover:bg-blue-400 cursor-pointer'>Create account</button> }
       </div>
+      </div>
+      
 
       {/* for mobile tab */}
       <div>  </div>
